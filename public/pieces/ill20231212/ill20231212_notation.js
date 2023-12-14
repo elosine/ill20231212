71a10614a139729d33f279cb2c24d3c761c6af43
elosine <justinwenloyang@gmail.com>
2023-12-13 23:05:09 -0500

    u

--- public/pieces/ill20231212/ill20231212_notation.js
+++ public/pieces/ill20231212/ill20231212_notation.js
@@ -1,29 +1,289 @@
 //#ef NOTES
 /*
-Go through and redoo timelineFRAMES check 105px per beat
-one beat takes x frames
-60bpm/1bps
-60 frames
-1.75 pixels per frame
-pixel counter: advance this many pixels per frame; float counter
-in update, last minute round to the nearest int in pixel counter
-
-Calculate BB Timing
-Animate/Update BBs
-Animated Cursor
-Add Bouncing Ball to Cursor
+Figure Out BB
 */
 //#endef NOTES
 
-
-
-//#ef GLOBAL VARIABLES
-
-
 //#ef General Variables
-let NUM_PLAYERS = 1;
 const TEMPO_COLORS = [clr_brightOrange, clr_brightGreen, clr_brightBlue, clr_lavander, clr_darkRed2];
+//Timing
+const FRAMERATE = 60;
+const MS_PER_FRAME = 1000.0 / FRAMERATE;
+let FRAMECOUNT = 0;
+//Canvas Variables
+let WORLD_W; // Calculated later in notation variables; 9 beats of notation @ 105 pixels per beat
+let WORLD_H; //calculated later with notation variables
+let canvas = {}; // canvas.panel(jspanel); canvas.div(jspanel div); canvas.svg(svg container on top of canvas.div)
+let panelTitle = "Interactive Looping Line 20231212"
+//Notation Variables
+//File name LL20231212_SVG.svg; long single line svg notation with proportionate spacing
+//just draw several times 1 for each line and move it over
+//105 pixels per beat; 42 beats; 4410 x 109
+//9 beats per line; 945 pixels per line
+const NOTATION_FILE_NAME = 'ILL20231212_SVG.svg';
+const PX_PER_BEAT = 105;
+const BEATS_PER_LINE = 9;
+const WHOLE_NOTATION_W = 4410;
+const NOTATION_H = 109;
+const GAP_BTWN_NOTATION_LINES = 10;
+const VERT_DISTANCE_BETWEEN_LINES = NOTATION_H + GAP_BTWN_NOTATION_LINES;
+const NOTATION_TOTAL_BEATS = 42; //cut off a few measures for long note but keep 42 for pixels per beat
+const NUM_NOTATION_LINES = 4;
+const NUM_BEATS_IN_PIECE = 36;
+const NOTATION_LINE_LENGTH = BEATS_PER_LINE * PX_PER_BEAT;
+const TOTAL_NUM_PX_IN_SCORE = NOTATION_LINE_LENGTH * NUM_NOTATION_LINES;
+WORLD_W = NOTATION_LINE_LENGTH;
+WORLD_H = (NOTATION_H * NUM_NOTATION_LINES) + (GAP_BTWN_NOTATION_LINES * (NUM_NOTATION_LINES - 1));
+//Beat Lines
+let beatLines = [];
+//Scrolling Cursors
+let scrollingCursors = [];
+let scrollingCsrY1 = 33;
+let scrollingCsrH = 57;
+let scrollingCsrClrs = [clr_limeGreen, clr_mustard, clr_brightOrange, clr_brightBlue];
+//Tempo Timing
+let tempos = [60, 37.14, 96.92];
+let totalNumFramesPerTempo = [];
+let tempoConsts = [];
+tempos.forEach((tempo, i) => {
+  let td = {};
+  td['bpm'] = tempo;
+  let bps = tempo / 60;
+  td['bps'] = bps;
+  let framesPerBeat = FRAMERATE / bps;
+  td['framesPerBeat'] = framesPerBeat;
+  let numFramesInPiece = NUM_BEATS_IN_PIECE * framesPerBeat;
+  td['numFramesInPiece'] = numFramesInPiece;
+  let pxPerFrame = PX_PER_BEAT / framesPerBeat;
+  td['pxPerFrame'] = pxPerFrame;
+  td['framesPerLine'] = framesPerBeat * BEATS_PER_LINE;
+  tempoConsts.push(td);
+});
+//Timesync
+const TS = timesync.create({
+  server: '/timesync',
+  interval: 1000
+});
 //#endef General Variables
+
+//#ef Animation Engine
+//Animation Engine Variables
+let cumulativeChangeBtwnFrames_MS = 0;
+let epochTimeOfLastFrame_MS;
+
+function animationEngine(timestamp) { //timestamp not used; timeSync server library used instead
+  let ts_Date = new Date(TS.now()); //Date stamp object from TimeSync library
+  let tsNowEpochTime_MS = ts_Date.getTime();
+  cumulativeChangeBtwnFrames_MS += tsNowEpochTime_MS - epochTimeOfLastFrame_MS;
+  epochTimeOfLastFrame_MS = tsNowEpochTime_MS; //update epochTimeOfLastFrame_MS for next frame
+  while (cumulativeChangeBtwnFrames_MS >= MS_PER_FRAME) { //if too little change of clock time will wait until 1 animation frame's worth of MS before updating etc.; if too much change will update several times until caught up with clock time
+    if (cumulativeChangeBtwnFrames_MS > (MS_PER_FRAME * FRAMERATE)) cumulativeChangeBtwnFrames_MS = MS_PER_FRAME; //escape hatch if more than 1 second of frames has passed then just skip to next update according to clock
+    update();
+    FRAMECOUNT++;
+    cumulativeChangeBtwnFrames_MS -= MS_PER_FRAME; //subtract from cumulativeChangeBtwnFrames_MS 1 frame worth of MS until while cond is satisified
+  }
+  requestAnimationFrame(animationEngine);
+}
+// Update Functions
+function update() {
+  totalNumFramesPerTempo.forEach((numFrames, tempoIx) => {
+    let currFrame = FRAMECOUNT % numFrames;
+    updateScrollingCsrs(currFrame, tempoIx);
+  });
+
+}
+//#endef Animation Engine END
+
+//#ef Calculate Timelines
+function calcTimeline() {
+  //Number of frames in score
+  tempoConsts.forEach((tempoObj, tempoIx) => { //run for each tempo
+    let tNumFrames = Math.round(tempoObj.numFramesInPiece); //create an array with and index for each frame in the piece
+    let tFrmsPerLine = tempoObj.framesPerLine;
+    let frameArray = [];
+    for (var frmIx = 0; frmIx < tNumFrames; frmIx++) { //loop for each frame in the piece
+      let td = {};
+      let tCurPx = Math.round(frmIx * tempoObj.pxPerFrame);
+      let tx = tCurPx % NOTATION_LINE_LENGTH; //calculate cursor x location at each frame for this tempo
+      td['x'] = tx;
+      //Calc Y pos
+      let ty;
+      if (frmIx < tFrmsPerLine) {
+        ty = scrollingCsrY1
+      } else if (frmIx >= tFrmsPerLine && frmIx < (tFrmsPerLine * 2)) {
+        ty = scrollingCsrY1 + NOTATION_H + GAP_BTWN_NOTATION_LINES;
+      } else if (frmIx >= (tFrmsPerLine * 2) && frmIx < (tFrmsPerLine * 3)){
+        ty = scrollingCsrY1 + ((NOTATION_H+GAP_BTWN_NOTATION_LINES) * 2);
+      } else{
+        ty = scrollingCsrY1 + ((NOTATION_H+GAP_BTWN_NOTATION_LINES) * 3);
+      }
+      td['y'] = ty;
+      frameArray.push(td);
+    }
+    tempoConsts[tempoIx]['frameArray'] = frameArray;
+    totalNumFramesPerTempo.push(frameArray.length);
+  });
+}
+//#endef Calculate Timelines
+
+//#ef INIT
+function init() { //runs from html file: ill20231212.html <body onload='init();'></body>
+  makeCanvas();
+  drawNotation();
+  makeScrollingCursors();
+  calcTimeline();
+  // console.log(tempoConsts);
+  //Initialize clock and start animation engine
+  let ts_Date = new Date(TS.now()); //Date stamp object from TimeSync library
+  let tsNowEpochTime_MS = ts_Date.getTime(); //current time at init in Epoch Time MS
+  epochTimeOfLastFrame_MS = tsNowEpochTime_MS;
+  requestAnimationFrame(animationEngine); //kick off animation
+}
+//#endef INIT
+
+//#ef Make Canvas
+function makeCanvas() {
+  //Make Panel with jsPanel
+  let tPanel = mkPanel({
+    w: WORLD_W,
+    h: WORLD_H,
+    title: panelTitle,
+    onwindowresize: true,
+    clr: 'none',
+    ipos: 'center-top',
+  });
+  // Enable Click/Tap to go to full screen mode
+  tPanel.content.addEventListener('click', function() {
+    document.documentElement.webkitRequestFullScreen({
+      navigationUI: 'hide'
+    });
+  });
+  //tPanel.content is the jspanel's div container
+  //Change Background Color of Panel: tPanel.content.style.backgroundColor = clr_plum;
+  canvas['panel'] = tPanel;
+  canvas['div'] = tPanel.content;
+  // SVG Container on top of jsPanel's div
+  let tSvg = mkSVGcontainer({
+    canvas: tPanel.content,
+    w: WORLD_W,
+    h: WORLD_H,
+    x: 0,
+    y: 0,
+  });
+  //Change Background Color of svg container tSvg.style.backgroundColor = clr_mustard
+  canvas['svg'] = tSvg;
+}
+//#endef Make Canvas
+
+//#ef Draw Notation
+function drawNotation() {
+  for (var i = 0; i < NUM_NOTATION_LINES; i++) {
+    //Notation
+    let tSvgImage = document.createElementNS(SVG_NS, "image");
+    tSvgImage.setAttributeNS(XLINK_NS, 'xlink:href', '/pieces/ill20231212/notationSVGs/' + NOTATION_FILE_NAME);
+    tSvgImage.setAttributeNS(null, "y", i * (NOTATION_H + GAP_BTWN_NOTATION_LINES));
+    tSvgImage.setAttributeNS(null, "x", i * -NOTATION_LINE_LENGTH);
+    tSvgImage.setAttributeNS(null, "visibility", 'visible');
+    tSvgImage.setAttributeNS(null, "display", 'yes');
+    canvas.svg.appendChild(tSvgImage);
+    //Beat Lines
+    for (var j = 0; j < BEATS_PER_LINE; j++) {
+      let tx2 = j * PX_PER_BEAT;
+      let y1 = (i * VERT_DISTANCE_BETWEEN_LINES);
+      let tBl = mkSvgLine({
+        svgContainer: canvas.svg,
+        x1: tx2,
+        y1: y1,
+        x2: tx2,
+        y2: y1 + NOTATION_H,
+        stroke: 'magenta',
+        strokeW: 0.5
+      });
+      beatLines.push(tBl);
+    }
+  }
+}
+//#endef Draw Notation
+
+//##ef Scrolling Cursor
+function makeScrollingCursors() {
+  for (var i = 0; i < tempos.length; i++) {
+    let tCsr = mkSvgLine({
+      svgContainer: canvas.svg,
+      x1: 0,
+      y1: scrollingCsrY1,
+      x2: 0,
+      y2: scrollingCsrY1 + scrollingCsrH,
+      stroke: scrollingCsrClrs[i],
+      strokeW: 2
+    });
+    tCsr.setAttributeNS(null, 'stroke-linecap', 'round');
+    tCsr.setAttributeNS(null, 'display', 'yes');
+    scrollingCursors.push(tCsr);
+  }
+}
+//Animate Scrolling Cursors
+function updateScrollingCsrs(frame, tempoIx) {
+  let tx = tempoConsts[tempoIx].frameArray[frame].x;
+  let ty = tempoConsts[tempoIx].frameArray[frame].y;
+  scrollingCursors[tempoIx].setAttributeNS(null, 'x1', tx);
+  scrollingCursors[tempoIx].setAttributeNS(null, 'x2', tx);
+  scrollingCursors[tempoIx].setAttributeNS(null, 'y1', ty);
+  scrollingCursors[tempoIx].setAttributeNS(null, 'y2', ty + scrollingCsrH);
+  //Scrolling BB
+  // cursorBBs[0].setAttributeNS(null, 'transform', "translate(" + currScrollingCsrX.toString() + "," + currScrollingCsrY.toString() + ")");
+  // let tBBy = timelineFRAMES[timelineFrameIx].bby;
+  // cursorBBs[0].setAttributeNS(null, 'cx', currScrollingCsrX);
+  // cursorBBs[0].setAttributeNS(null, 'cy', tBBy + currScrollingCsrY);
+}
+//##endef Scrolling Cursor
+
+
+
+
+
+
+
+
+/*
+
+for (var i = 0; i < NUM_NOTATION_LINES; i++) {
+
+  let ty = -18 + (i * VERT_DISTANCE_BETWEEN_LINES);
+  let tx1 = -2 + (i * -944);
+  let tSvgImage = document.createElementNS(SVG_NS, "image");
+  tSvgImage.setAttributeNS(XLINK_NS, 'xlink:href', '/pieces/ill20231212/notationSVGs/ILL20231212_SVG.svg');
+  tSvgImage.setAttributeNS(null, "y", ty);
+  tSvgImage.setAttributeNS(null, "x", tx1);
+  tSvgImage.setAttributeNS(null, "visibility", 'visible');
+  tSvgImage.setAttributeNS(null, "display", 'yes');
+  staves[0].svg.appendChild(tSvgImage);
+
+
+
+  //End of line mask
+  let tLineMask = mkSvgRect({
+    svgContainer: staves[0].svg,
+    x: 944,
+    y: i * 105,
+    w: 70,
+    h: 70,
+    fill: 'white',
+    stroke: 'none',
+    strokeW: 0,
+    roundR: 0
+  });
+
+}
+*/
+
+
+
+
+
+
+
+/*
 
 //##ef Timing
 const FRAMERATE = 60;
@@ -53,32 +313,13 @@ const PX_PER_FRAME = PX_PER_SEC / FRAMERATE;
 const BEAT_DUR_FRAMES = PX_PER_BEAT / PX_PER_FRAME; //Width of the staff in frames
 //##endef Timing
 
-//#ef Animation Engine Variables
-let cumulativeChangeBtwnFrames_MS = 0;
-let epochTimeOfLastFrame_MS;
-let animationEngineCanRun = true;
-//#endef END Animation Engine Variables
 
-//#ef TIMESYNC
-const TS = timesync.create({
-  server: '/timesync',
-  interval: 1000
-});
-//#endef TIMESYNC
+
+
 
 //#ef World Panel Variables
-let worldPanel;
-const DEVICE_SCREEN_W = window.screen.width;
-const DEVICE_SCREEN_H = window.screen.height;
-const MAX_W = 1200; //16:10 aspect ratio; 0.625
-const MAX_H = 800;
-const WORLD_MARGIN = 10;
-// const WORLD_W = Math.min(DEVICE_SCREEN_W, MAX_W) - (WORLD_MARGIN * 2);
-// const WORLD_H = Math.min(DEVICE_SCREEN_H, MAX_H) - 45;
-const WORLD_W = 950;
-const WORLD_H = 450;
-const WORLD_CENTER = WORLD_W / 2;
-const GAP = 6;
+
+
 //#endef World Panel Variables
 
 //#ef Canvas Variables
@@ -89,7 +330,7 @@ const NOTATIONCANVAS_W = WORLD_W;
 
 //#ef Staff Variables
 const NUMSTAVES = 1;
-const NUMLINES = 4;
+const NUM_NOTATION_LINES = 4;
 const STAFFGAP = 4;
 const STAFF_H = 700;
 const STAFF_W = NOTATIONCANVAS_W;
@@ -152,7 +393,7 @@ for (var i = 0; i < STAFF_WIDTH_IN_MS; i++) {
 
 //Should contain an index for each frame in the timeline
 let tJ = 0;
-for (var i = 0; i < (STAFF_WIDTH_IN_FRAMES * NUMLINES); i++) {
+for (var i = 0; i < (STAFF_WIDTH_IN_FRAMES * NUM_NOTATION_LINES); i++) {
   let tFrameDict = {}; //a dictionary for each frame
   let tpx = Math.round(i % STAFF_WIDTH_IN_FRAMES * PX_PER_FRAME); //this is the pixel location for each frame; modulo for each line
   tFrameDict['x'] = tpx;
@@ -173,141 +414,19 @@ console.log(timelineFRAMES);
 
 
 
-//#endef GLOBAL VARIABLES
 
 
 
-//#ef INIT
-function init() {
-
-  makeWorldPanel();
-  makeStaves();
-  drawNotation();
-  makeScrollingTempoCursors();
-  makeScrollingCursorBbs();
-
-  let ts_Date = new Date(TS.now()); //Date stamp object from TimeSync library
-  let tsNowEpochTime_MS = ts_Date.getTime();
-  epochTimeOfLastFrame_MS = tsNowEpochTime_MS;
-  requestAnimationFrame(animationEngine); //kick off animation
-
-} // function init() END
-//#endef INIT
 
 
 
 //#ef BUILD WORLD
 
 
-//#ef Make World Panel - floating window made in jspanel
-function makeWorldPanel() {
-  worldPanel = mkPanel({
-    w: WORLD_W,
-    h: WORLD_H,
-    title: 'SoundFlow #5',
-    onwindowresize: true,
-    clr: 'none',
-    ipos: 'center-top',
-  });
 
-  worldPanel.content.addEventListener('click', function() {
-    document.documentElement.webkitRequestFullScreen({
-      navigationUI: 'hide'
-    });
-  });
 
-} // function makeWorldPanel() END
-//#endef Make World Panel
 
-//#ef Make Staves - SVG rectangle for each individual staff (draw notation on top)
-function makeStaves() {
 
-  for (var i = 0; i < NUMSTAVES; i++) {
-    let tStaffObj = {}; //{div:,svg:,rect:}
-    let ty = i * (STAFF_H + STAFFGAP);
-
-    let tDiv = mkDiv({
-      canvas: worldPanel.content,
-      w: STAFF_W,
-      h: STAFF_H,
-      top: ty,
-      left: 0,
-      bgClr: 'white'
-    });
-
-    tStaffObj['div'] = tDiv;
-
-    let tSvg = mkSVGcontainer({
-      canvas: tDiv,
-      w: STAFF_W,
-      h: STAFF_H,
-      x: 0,
-      y: 0,
-      clr: 'white'
-    });
-
-    tStaffObj['svg'] = tSvg;
-
-    staves.push(tStaffObj);
-
-  } // for (var i = 0; i < NUMSTAVES; i++) END
-
-} // function makeStaves() END
-//#endef Make Staves
-
-//#ef Draw Notation SVG
-function drawNotation() {
-
-  for (var i = 0; i < NUMLINES; i++) {
-
-    let ty = -18 + (i * VERT_DISTANCE_BETWEEN_LINES);
-    let tx1 = -2 + (i * -944);
-    let tSvgImage = document.createElementNS(SVG_NS, "image");
-    tSvgImage.setAttributeNS(XLINK_NS, 'xlink:href', '/pieces/ill20231212/notationSVGs/ILL20231212_SVG.svg');
-    tSvgImage.setAttributeNS(null, "y", ty);
-    tSvgImage.setAttributeNS(null, "x", tx1);
-    tSvgImage.setAttributeNS(null, "visibility", 'visible');
-    tSvgImage.setAttributeNS(null, "display", 'yes');
-    staves[0].svg.appendChild(tSvgImage);
-
-    //Beat Lines
-    for (var j = 0; j < 9; j++) {
-      let tx2 = j * PX_PER_BEAT;
-      let y2 = (i * VERT_DISTANCE_BETWEEN_LINES) + BEAT_LINES_Y1;
-      let tBl = mkSvgLine({
-        svgContainer: staves[0].svg,
-        x1: tx2,
-        y1: y2,
-        x2: tx2,
-        y2: y2 + 55,
-        stroke: 'magenta',
-        strokeW: 0.5
-      });
-      beatLines.push(tBl);
-      let tBeatLinesXYdict = {};
-      tBeatLinesXYdict['x'] = tx2;
-      tBeatLinesXYdict['y'] = y2 + BEAT_LINES_H;
-      beatLines_XY.push(tBeatLinesXYdict);
-
-    }
-
-    //End of line mask
-    let tLineMask = mkSvgRect({
-      svgContainer: staves[0].svg,
-      x: 944,
-      y: i * 105,
-      w: 70,
-      h: 70,
-      fill: 'white',
-      stroke: 'none',
-      strokeW: 0,
-      roundR: 0
-    });
-
-  }
-
-}
-//#endef Draw Notation SVG
 
 //##ef Make Scrolling Tempo Cursors
 
@@ -385,7 +504,7 @@ function updateScrollingCsrs() {
 } // function updateScrollingCsrs() END
 //###endef updateScrollingCsrs
 
-/*
+
 //###ef updateScrollingBBs
 function updateScrollingBBs() {
   if (FRAMECOUNT > 0) { //No lead in motion for scrolling cursors
@@ -409,7 +528,7 @@ function updateScrollingBBs() {
   } // if (FRAMECOUNT > LEAD_IN_FRAMES) END
 } // function updateScrollingCsrs() END
 //###endef updateScrollingBBs
-*/
+
 
 //#endef WIPE/UPDATE/DRAW
 
@@ -418,30 +537,6 @@ function updateScrollingBBs() {
 //#ef ANIMATION
 
 
-//#ef Animation Engine
-function animationEngine(timestamp) { //timestamp not used; timeSync server library used instead
-
-  let ts_Date = new Date(TS.now()); //Date stamp object from TimeSync library
-  let tsNowEpochTime_MS = ts_Date.getTime();
-  cumulativeChangeBtwnFrames_MS += tsNowEpochTime_MS - epochTimeOfLastFrame_MS;
-  epochTimeOfLastFrame_MS = tsNowEpochTime_MS; //update epochTimeOfLastFrame_MS for next frame
-
-  while (cumulativeChangeBtwnFrames_MS >= MS_PER_FRAME) { //if too little change of clock time will wait until 1 animation frame's worth of MS before updating etc.; if too much change will update several times until caught up with clock time
-
-    if (cumulativeChangeBtwnFrames_MS > (MS_PER_FRAME * FRAMERATE)) cumulativeChangeBtwnFrames_MS = MS_PER_FRAME; //escape hatch if more than 1 second of frames has passed then just skip to next update according to clock
-    wipe();
-    update();
-    draw();
-
-    FRAMECOUNT++;
-    cumulativeChangeBtwnFrames_MS -= MS_PER_FRAME; //subtract from cumulativeChangeBtwnFrames_MS 1 frame worth of MS until while cond is satisified
-
-  } // while (cumulativeChangeBtwnFrames_MS >= MS_PER_FRAME) END
-
-  if (animationEngineCanRun) requestAnimationFrame(animationEngine); //animation engine gate: animationEngineCanRun
-
-} // function animationEngine(timestamp) END
-//#endef Animation Engine END
 
 
 //#ef WIPE/UPDATE/DRAW
@@ -453,12 +548,7 @@ function wipe(epochClock_MS) {
 } // function wipe() END
 //#endef Wipe Function
 
-//#ef Update Function
-function update() {
-  updateScrollingCsrs();
 
-}
-//#endef Update Function
 
 //#ef Draw Function
 function draw(epochClock_MS) {
@@ -473,7 +563,7 @@ function draw(epochClock_MS) {
 //#endef ANIMATION
 
 
-
+*/
 
 
 //
